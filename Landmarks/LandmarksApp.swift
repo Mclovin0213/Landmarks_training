cbdf80d5cebc527a7728c2d368601043e025e862
Mclovin0213 <javellaneda0213@gmail,com>
2024-12-09 22:22:15 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by julian avellaneda on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
