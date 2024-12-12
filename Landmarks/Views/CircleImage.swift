1eb96849c0cdf8d330bdfa0d8a7c9ec174cb9260
Mclovin0213 <javellaneda0213@gmail.com>
2024-12-12 10:06:09 -0800

    add images, and add ModelData

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by julian avellaneda on 12/9/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
