27cbfc044d7b6b2769e4518494c429be890e41bf
Mclovin0213 <javellaneda0213@gmail.com>
2024-12-21 00:15:57 -0800

    add MacOs Target and new Landmark Detail view

---
+++ MacLandmarksTests/MacLandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksTests.swift
+//  MacLandmarksTests
+//
+//  Created by julian avellaneda on 12/20/24.
+//
+
+import Testing
+@testable import MacLandmarks
+
+struct MacLandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
